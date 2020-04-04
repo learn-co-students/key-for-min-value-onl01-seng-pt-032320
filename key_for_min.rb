e205a1a5884b081f00bc9a24caccc125cfef3b24
Evan Smith <professionalemail@zoho.com>
2020-04-04 08:29:02 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,22 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  first_run = true
+  lowest = 0
+  the_key = nil
 
+  name_hash.each do |key, value|
+    if first_run == true
+      lowest = value
+      the_key = key
+      first_run = false
+    else
+      if value < lowest
+      lowest = value
+      the_key = key
+      end
+    end
+  end
+  
+ the_key
 end
