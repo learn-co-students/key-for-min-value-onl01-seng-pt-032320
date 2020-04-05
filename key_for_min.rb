7afcccf33f1af803f4cbb41edb2fe13f658c7ef5
puckish-system-8282 <bryce.boddy@gmail.com>
2020-04-05 11:49:12 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  returnKey = nil
+  returnValue = nil
+  name_hash.each { |key, value|
+    if returnValue == nil or value < returnValue
+      returnKey = key
+      returnValue = value
+    end
+  }
+  returnKey
 end
